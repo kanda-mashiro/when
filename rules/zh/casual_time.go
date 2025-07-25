e8dcb2506aeb7a6cd0ac97cba551717683d97cea
o7si <o7si@o7sideMac-mini.local>
2025-07-25 23:51:19 +0800

    feat: expand time period recognition, add 早上、上午、夜晚 and other descriptions

--- rules/zh/casual_time.go
+++ rules/zh/casual_time.go
@@ -13,7 +13,7 @@ func CasualTime(s rules.Strategy) rules.Rule {
 	overwrite := s == rules.Override
 
 	return &rules.F{
-		RegExp: regexp.MustCompile(`(?i)(?:\W|^)((今天)?\s*(早晨|下午|傍晚|中午|晚上))`),
+		RegExp: regexp.MustCompile(`(?i)(?:\W|^)((今天)?\s*(早晨|早上|上午|中午|下午|傍晚|夜晚|晚上))`),
 		Applier: func(m *rules.Match, c *rules.Context, o *rules.Options, ref time.Time) (bool, error) {
 
 			lower := strings.ToLower(strings.TrimSpace(m.String()))
@@ -23,11 +23,32 @@ func CasualTime(s rules.Strategy) rules.Rule {
 			}
 
 			switch {
-			case strings.Contains(lower, "晚上"):
-				if o.Evening != 0 {
-					c.Hour = &o.Evening
+			case strings.Contains(lower, "早晨"):
+				if o.Morning != 0 {
+					c.Hour = &o.Morning
 				} else {
-					c.Hour = pointer.ToInt(20)
+					c.Hour = pointer.ToInt(8)
+				}
+				c.Minute = pointer.ToInt(0)
+			case strings.Contains(lower, "早上"):
+				if o.Morning != 0 {
+					c.Hour = &o.Morning
+				} else {
+					c.Hour = pointer.ToInt(8)
+				}
+				c.Minute = pointer.ToInt(0)
+			case strings.Contains(lower, "上午"):
+				if o.Morning != 0 {
+					c.Hour = &o.Morning
+				} else {
+					c.Hour = pointer.ToInt(10)
+				}
+				c.Minute = pointer.ToInt(0)
+			case strings.Contains(lower, "中午"):
+				if o.Noon != 0 {
+					c.Hour = &o.Noon
+				} else {
+					c.Hour = pointer.ToInt(12)
 				}
 				c.Minute = pointer.ToInt(0)
 			case strings.Contains(lower, "下午"):
@@ -44,18 +65,18 @@ func CasualTime(s rules.Strategy) rules.Rule {
 					c.Hour = pointer.ToInt(18)
 				}
 				c.Minute = pointer.ToInt(0)
-			case strings.Contains(lower, "早晨"):
-				if o.Morning != 0 {
-					c.Hour = &o.Morning
+			case strings.Contains(lower, "夜晚"):
+				if o.Evening != 0 {
+					c.Hour = &o.Evening
 				} else {
-					c.Hour = pointer.ToInt(8)
+					c.Hour = pointer.ToInt(21)
 				}
 				c.Minute = pointer.ToInt(0)
-			case strings.Contains(lower, "中午"):
-				if o.Noon != 0 {
-					c.Hour = &o.Noon
+			case strings.Contains(lower, "晚上"):
+				if o.Evening != 0 {
+					c.Hour = &o.Evening
 				} else {
-					c.Hour = pointer.ToInt(12)
+					c.Hour = pointer.ToInt(21)
 				}
 				c.Minute = pointer.ToInt(0)
 			}
